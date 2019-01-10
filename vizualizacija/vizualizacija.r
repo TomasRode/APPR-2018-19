0877ac1d1cd1df01e8e07a48b7654d8988cae3da
TomasRode <39487154+TomasRode@users.noreply.github.com>
2019-01-10 10:02:50 +0100

    razlike

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -3,14 +3,12 @@
 library(ggplot2)
 library(rgdal)
 library(rgeos)
-# library(mosaic)
-# library(maptools)
 source('uvoz/uvoz.r')
 
 PISA.povprecje <- PISA %>% filter(SUBJECT=='TOT') %>% group_by(TIME, LOCATION) %>% summarise(POVPRECJE=mean(Value))
 PISApov <- ggplot(PISA.povprecje, aes(x=TIME,y=POVPRECJE, color=LOCATION)) + geom_line()
 
-#NE DOBIMO NIC POSEBNEGA, MEDCASOVNA PRIMERJAVA NI SMISELNO, KER JE TESTE TEZKO SESTAVITI VSAKO LETO ENAKE
+#NE DOBIMO NIC POSEBNEGA, MEDCASOVNA PRIMERJAVA NI SMISELNA, KER JE TESTE TEZKO SESTAVITI VSAKO LETO ENAKE
 #ZATO LAHKO DELAMO SAMO PRIMERJAVE MED DRZAVAMI
 
 PISA.BDPPC <- PISA %>% filter(SUBJECT=='TOT') %>% merge(BDPpc)
@@ -46,13 +44,11 @@ source('lib/uvozi.zemljevid.r')
 zemljevid <- uvozi.zemljevid("https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/110m/cultural/110m_cultural.zip",
                              "ne_110m_admin_0_countries", encoding="Windows-1250") %>% fortify
 
-ggplot() + geom_polygon(data=zemljevid,aes(x=long,y=lat,group=group))
+
+Pzem <- left_join(zemljevid, PISA.povprecje, by = c('ADM0_A3_US'='LOCATION'))
+drzaveZ <- sort(unique(zemljevid$ADM0_A3_US))
+razlike <- unique(PISA.povprecje$LOCATION)[!(unique(PISA.povprecje$LOCATION)%in%unique(Pzem$ADM0_A3_US))]
+# težava: zemljevid nima vnešenih sledečih držav: HONG-KONG, SINGAPUR, MACAO
+ggplot() + geom_polygon(data=Pzem, aes(x=long,y=lat,group=group, fill=POVPRECJE ))
 
 
- # levels(zemljevid$OB_UIME) <- levels(zemljevid$OB_UIME) %>%
- #   { gsub("Slovenskih", "Slov.", .) } %>% { gsub("-", " - ", .) }
- # zemljevid$OB_UIME <- factor(zemljevid$OB_UIME, levels=levels(obcine$obcina))
-
-# Izračunamo povprečno velikost družine
-# povprecja <- druzine %>% group_by(obcina) %>%
-#   summarise(povprecje=sum(velikost.druzine * stevilo.druzin) / sum(stevilo.druzin))
