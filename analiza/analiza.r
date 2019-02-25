966e909080f383f50061c62ba235bbbf655b2a0f
TomasRode <39487154+TomasRode@users.noreply.github.com>
2019-02-25 16:32:18 +0100

    podatki za razvrscanje

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,11 +1,16 @@
 # 4. faza: Analiza podatkov
 
-podatki <- obcine %>% transmute(obcina, povrsina, gostota,
-                                gostota.naselij=naselja/povrsina) %>%
-  left_join(povprecja, by="obcina")
-row.names(podatki) <- podatki$obcina
-podatki$obcina <- NULL
+PodatkiRazvrscanje <- PISA %>% group_by(LOCATION, SUBJECT) %>% summarise(POVPRECJE=mean(Value))%>% dcast(LOCATION ~ SUBJECT) %>% 
+                          merge(BDPpc %>% group_by(LOCATION) %>% summarise(PovBDPpc=mean(BDPpc, na.rm=TRUE))) %>%
+                          merge(ObveznoSol %>% group_by(LOCATION) %>% summarise(PovpObveznaLeta=mean(ObveznaLeta, na.rm=TRUE))) %>% 
+                          merge(RazmerjeOS %>% group_by(LOCATION) %>% summarise(PovpRazmerjeOS=mean(RazmerjeOS, na.rm=TRUE))) %>% 
+                          merge(RazmerjeSS %>% group_by(LOCATION) %>% summarise(PovpRazmerjeSS=mean(RazmerjeSS, na.rm=TRUE))) %>%
+                          merge(ZaIzob %>% group_by(LOCATION) %>% summarise(PovpZaIzob=mean(PotrosnjaZaIzob, na.rm=TRUE))) %>%
+                          merge(ZaOsebje %>% group_by(LOCATION) %>% summarise(PovpZaOseb=mean(PotrosnjaZaOsebje, na.rm=TRUE)))
+
+Model <- hclust(dist(scale(PodatkiRazvrscanje)))
+  
 
 # Število skupin
-n <- 5
-skupine <- hclust(dist(scale(podatki))) %>% cutree(n)
+# n <- 5
+# skupine <- hclust(dist(scale(podatki))) %>% cutree(n)
