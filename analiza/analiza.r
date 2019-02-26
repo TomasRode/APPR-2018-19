32e7b898ccf5f7e96e04920bc9eee4296a21eb80
TomasRode <39487154+TomasRode@users.noreply.github.com>
2019-02-26 18:48:12 +0100

    razvrščanje

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,16 +1,28 @@
 # 4. faza: Analiza podatkov
+require(ggdendro)
+
 
 PodatkiRazvrscanje <- PISA %>% group_by(LOCATION, SUBJECT) %>% summarise(POVPRECJE=mean(Value))%>% dcast(LOCATION ~ SUBJECT) %>% 
                           merge(BDPpc %>% group_by(LOCATION) %>% summarise(PovBDPpc=mean(BDPpc, na.rm=TRUE))) %>%
                           merge(ObveznoSol %>% group_by(LOCATION) %>% summarise(PovpObveznaLeta=mean(ObveznaLeta, na.rm=TRUE))) %>% 
-                          merge(RazmerjeOS %>% group_by(LOCATION) %>% summarise(PovpRazmerjeOS=mean(RazmerjeOS, na.rm=TRUE))) %>% 
-                          merge(RazmerjeSS %>% group_by(LOCATION) %>% summarise(PovpRazmerjeSS=mean(RazmerjeSS, na.rm=TRUE))) %>%
+                          merge(RazmerjeOS %>% group_by(LOCATION) %>% summarise(PovpRazmerjeOS=mean(RazmerjeUUOS, na.rm=TRUE))) %>% 
+                          merge(RazmerjeSS %>% group_by(LOCATION) %>% summarise(PovpRazmerjeSS=mean(RazmerjeUUSS, na.rm=TRUE))) %>%
                           merge(ZaIzob %>% group_by(LOCATION) %>% summarise(PovpZaIzob=mean(PotrosnjaZaIzob, na.rm=TRUE))) %>%
                           merge(ZaOsebje %>% group_by(LOCATION) %>% summarise(PovpZaOseb=mean(PotrosnjaZaOsebje, na.rm=TRUE)))
 
+rownames(PodatkiRazvrscanje) <- PodatkiRazvrscanje$LOCATION
+PodatkiRazvrscanje <- PodatkiRazvrscanje[-1]
+
 Model <- hclust(dist(scale(PodatkiRazvrscanje)))
+
+ggdendrogram(Model)
   
 
 # Število skupin
-# n <- 5
-# skupine <- hclust(dist(scale(podatki))) %>% cutree(n)
+n <- 4
+skupine <- hclust(dist(scale(PodatkiRazvrscanje))) %>% cutree(n)
+
+tabelaskupin <- data.frame(as.list(skupine))
+imena <- names(tabelaskupin)
+
+tabelaskupin <- data.frame(imena, skupine) %>% rename(LOCATION = imena, SKUPINE = skupine)
