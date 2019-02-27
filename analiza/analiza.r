d9dc878212338fbd27d209e1acd9380254dbd404
TomasRode <39487154+TomasRode@users.noreply.github.com>
2019-02-27 11:29:28 +0100

    Zemljevid skupin

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -19,10 +19,14 @@ ggdendrogram(Model)
   
 
 # Število skupin
-n <- 4
+n <- 7
 skupine <- hclust(dist(scale(PodatkiRazvrscanje))) %>% cutree(n)
 
 tabelaskupin <- data.frame(as.list(skupine))
 imena <- names(tabelaskupin)
 
 tabelaskupin <- data.frame(imena, skupine) %>% rename(LOCATION = imena, SKUPINE = skupine)
+rownames(tabelaskupin) <- c()
+
+Rzem <- left_join(zemljevid, tabelaskupin, by = c('ADM0_A3_US'='LOCATION'))
+Rzemljevid <- ggplot() + geom_polygon(data=Rzem, aes(x=long,y=lat,group=group, fill=factor(SKUPINE) ))
